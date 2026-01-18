677227d885c02fcf11dca970e59329d33adab49b
Sai Prasad Nayak <saiprasadnayak507@gmail.com>
2026-01-18 12:19:18 +0530

    Updated to accept form data

--- src/services/ResumeService.js
+++ src/services/ResumeService.js
@@ -6,9 +6,14 @@ const API = axios.create({
   baseURL: "https://resume-sensei-backend.onrender.com/"
 });
 
-export const analyzeResume = (resumeText) => {
-  return API.post("/api/resume/analyze", {
-    resumeText
+export const analyzeResume = (resumeText, file) => {
+  const formData = new FormData();
+  if (resumeText) formData.append("resumeText", resumeText);
+  if (file) formData.append("resumeFile", file);
+
+  return API.post("/api/resume/analyze", formData, {
+    headers: { "Content-Type": "multipart/form-data" }
   });
 };
+
 
